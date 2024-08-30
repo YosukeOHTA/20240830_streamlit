d45826469c8eaa3d2a9662fdefbcf151227dc189
YosukeOHTA <deaufe@gmail.com>
2024-08-30 22:41:52 +0900

    selenium

--- pages/p01_geidaiFes.py
+++ pages/p01_geidaiFes.py
@@ -6,13 +6,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('-no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(service=Service(), options=chrome_options)
+# driver = webdriver.Chrome(service=Service(), options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://tiget.net/users/1207563')
 time.sleep(4)
 
