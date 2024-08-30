549a7bd62d46d81cb1e66b5585c742cf4f59e6a2
YosukeOHTA <deaufe@gmail.com>
2024-08-30 23:24:58 +0900

    fasd

--- pages/p01_geidaiFes.py
+++ pages/p01_geidaiFes.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('-no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
